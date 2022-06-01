9304118f40d13776c005a5c89a15302bf00eb69d
wy-time <time_117@163.com>
2022-06-01 15:16:18 +0800

    修复segmentView的位置错误

--- Sources/GKPageSmoothView/GKPageSmoothView.m
+++ Sources/GKPageSmoothView/GKPageSmoothView.m
@@ -667,10 +667,6 @@ static NSString *const GKPageSmoothViewCellID = @"smoothViewCell";
         self.headerView.frame = CGRectMake(0, 0, size.width, self.headerHeight);
         self.segmentedView.frame =  CGRectMake(0, self.headerHeight, size.width, self.segmentedHeight);
         
-        if (self.segmentedView.superview != self.headerContainerView) { // 修复headerHeight < size.height, headerContainerHeight > size.height时segmentedView.superView为bottomContainerView
-            [self.headerContainerView addSubview:self.segmentedView];
-        }
-        
         if (!self.isMainScrollDisabled) {
             for (id<GKPageSmoothListViewDelegate> list in self.listDict.allValues) {
                 UIEdgeInsets temp = list.listScrollView.contentInset;
@@ -681,7 +677,7 @@ static NSString *const GKPageSmoothViewCellID = @"smoothViewCell";
         if (self.isBottomHover) {
             self.bottomContainerView.frame = CGRectMake(0, size.height - self.segmentedHeight, size.width, size.height - self.ceilPointHeight);
             
-            if (self.headerHeight > size.height && self.hoverType == GKPageSmoothHoverTypeBottom) {
+            if (self.headerContainerHeight > size.height && self.hoverType == GKPageSmoothHoverTypeBottom) {
                 self.bottomContainerView.hidden = NO; // 修复滑动到非悬浮状态后执行刷新导致bottomContainerView未显示的问题
                 self.segmentedView.frame = CGRectMake(0, 0, size.width, self.segmentedHeight);
                 [self.bottomContainerView addSubview:self.segmentedView];
